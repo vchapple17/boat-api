23d3b8d057f811e16480f121e471a2cbf5275b61
vchapple17 <vchapple17@gmail.com>
2018-01-26 14:29:06 -0600

    First route Test

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
-import datetime as date
 
-print date.datetime.now()
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain';
+        self.response.out.write('Hello, world')
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
